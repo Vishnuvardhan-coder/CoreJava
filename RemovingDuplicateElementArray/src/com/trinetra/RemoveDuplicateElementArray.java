6335445df1a0d2e83ce0de45bd64aea6e75f6659
Vishnu <Vishnu@192.168.0.4>
2020-10-12 13:02:58 +0530

    Remove Duplicate IN Array

---
+++ RemovingDuplicateElementArray/src/com/trinetra/RemoveDuplicateElementArray.java
@@ -0,0 +1,29 @@
+package com.trinetra;
+
+public class RemoveDuplicateElementArray {
+
+	public static void main(String[] args) {
+ 
+		int a[] = {1,2,2,3,4,5,5};
+		
+		int temp[] = new int[a.length];
+		
+		int j=0;
+		
+		for(int i = 0; i<a.length; i++){
+			
+			if(a[i]!=a[i+1]){
+				
+				temp[j]=a[i];	
+				j++;
+			}
+		}
+		temp[j] = a[a.length-1];
+		
+		for(int i=0; i<a.length; i++){
+			System.out.println(temp[i]+" ");
+		}
+		
+	}
+
+}
