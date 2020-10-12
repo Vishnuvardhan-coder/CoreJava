2e94899e0173ff15729f53cd625e59c1f617f23f
Vishnu <Vishnu@192.168.0.4>
2020-10-12 12:54:13 +0530

    EvenOdd

---
+++ EvenOdd/src/com/trinetra/Evenodd.java
@@ -0,0 +1,46 @@
+package com.trinetra;
+
+import java.util.Scanner;
+
+public class Evenodd {
+
+	/*public static void main(String[] args) {
+		
+	Scanner s = new Scanner(System.in);
+	
+	System.out.println("Enter a Number");
+	
+	int num = s.nextInt();
+	 if(num%2==0)
+	 {
+		 System.out.println(num+" Even Number");
+		 
+	 }
+	 
+	 else
+	 {
+		 
+		System.out.println(num+" Odd Number");
+	 }
+	 }
+	
+}*/
+
+	public class test {
+		public void main(String[] args) {
+		
+int[] a = new int[]{1,2,3,4};
+int[] reversedArray = new int[a.length];
+int j = 0;
+for (int i = a.length -1; i > 0; i--){
+    reversedArray[j++] = a[i];
+}
+
+	}}}
+
+
+
+
+
+
+
